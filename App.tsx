dfb4ae84a0f21e290d02f0fb7b22ea435dfdf364
Anastasiia Shyptia <shyptianastya27@gmail.com>
2023-04-14 10:57:05 +0300

    Add custom fonts to project

--- App.tsx
+++ App.tsx
@@ -17,13 +17,7 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -72,24 +66,12 @@ function App(): JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
+
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Section title="It's my first React Native project" />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -102,6 +84,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
   },
   sectionTitle: {
+    fontFamily: 'Montserrat',
     fontSize: 24,
     fontWeight: '600',
   },
@@ -109,8 +92,10 @@ const styles = StyleSheet.create({
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
+    fontFamily: 'Montserrat',
   },
   highlight: {
+    fontFamily: 'Montserrat',
     fontWeight: '700',
   },
 });
